b239b1a349c102f84394f2cc5c2a6776b2c52689
Lorenzo Bolla <lbolla@geneity.co.uk>
2010-08-20 14:04:44 +0100

    new colorscheme

---
+++ dwm/config.h
@@ -0,0 +1,124 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+// static const char font[]            = "-*-terminus-medium-r-normal-7-14-*-*-*-*-*-*-*";
+static const char font[]            = "-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*";
+
+#define BLACK "#242424"
+#define WHITE "#f6f3e8"
+#define BLUE  "#8ac6f2"
+
+static const char normbordercolor[] = BLACK;
+static const char normbgcolor[]     = WHITE;
+static const char normfgcolor[]     = BLUE;
+
+static const char selbordercolor[]  = BLUE;
+static const char selbgcolor[]      = BLUE;
+static const char selfgcolor[]      = WHITE;
+
+static unsigned int borderpx        = 1;        /* border pixel of windows */
+static unsigned int snap            = 32;       /* snap pixel */
+static Bool showbar                 = True;     /* False means no bar */
+static Bool topbar                  = True;     /* False means bottom bar */
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static Rule rules[] = {
+	/* class           instance      title         tags mask     isfloating */
+	{ "Xmessage",      NULL,         NULL,         0,            True },
+	{ "Mplayer",       NULL,         NULL,         0,            True },
+	{ "Xloadimage",    NULL,         NULL,         0,            True },
+	{ "Gimp",          NULL,         NULL,         0,            True },
+	{ "Pidgin",        NULL,         NULL,         0,            True },
+	{ "Vncviewer",     NULL,         NULL,         0,            True },
+	{ "Tsclient",      NULL,         NULL,         0,            True },
+	{ NULL,            NULL,         "floating",   0,            True },
+	// { "Iceweasel",     NULL,         NULL,         1 << 9,       False },
+	// { "google-chrome", NULL,         NULL,         1 << 9,       False },
+	// { NULL,            NULL,         NULL,         0,            False },
+};
+
+/* layout(s) */
+static float mfact      = 0.5; /* factor of master area size [0.05..0.95] */
+static Bool resizehints = True; /* False means respect size hints in tiled resizals */
+
+#include "grid.c"
+static Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "+++",      grid },
+};
+
+/* key definitions */
+#define MODKEY Mod1Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* commands */
+static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termfloatcmd[]  = { "urxvt", "-T", "floating", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
+static const char *chromecmd[]  = { "google-chrome", NULL };
+static const char *lockcmd[]  = { "slock", NULL };
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,           XK_c,      spawn,          {.v = chromecmd } },
+	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
+	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = termfloatcmd } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+};
+
+/* button definitions */
+/* click can be a tag number (starting at 0),
+ * ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
