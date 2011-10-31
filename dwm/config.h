237873d54fadb8df8dc8e9ce58e2148751e3843a
Lorenzo Bolla <lorenzo@zugoservices.com>
2011-10-31 10:11:42 +0000

    dwm configs

--- dwm/config.h
+++ dwm/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-// static const char font[]            = "-*-terminus-medium-r-normal-7-14-*-*-*-*-*-*-*";
-static const char font[]            = "-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*";
+static const char font[]            = "-*-terminus-medium-r-normal-7-14-*-*-*-*-*-*-*";
+// static const char font[]            = "-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*";
 
 #define BLACK "#242424"
 #define WHITE "#f6f3e8"
@@ -16,34 +16,27 @@ static const char selbordercolor[]  = BLUE;
 static const char selbgcolor[]      = BLUE;
 static const char selfgcolor[]      = BLACK;
 
-static unsigned int borderpx        = 1;        /* border pixel of windows */
-static unsigned int snap            = 32;       /* snap pixel */
-static Bool showbar                 = True;     /* False means no bar */
-static Bool topbar                  = True;     /* False means bottom bar */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const Bool showbar           = True;     /* False means no bar */
+static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static Rule rules[] = {
-	/* class           instance      title         tags mask     isfloating */
-	{ "Xmessage",      NULL,         NULL,         0,            True },
-	{ "Mplayer",       NULL,         NULL,         0,            True },
-	{ "Xloadimage",    NULL,         NULL,         0,            True },
-	{ "Gimp",          NULL,         NULL,         0,            True },
-	{ "Pidgin",        NULL,         NULL,         0,            True },
-	{ "Vncviewer",     NULL,         NULL,         0,            True },
-	{ "Tsclient",      NULL,         NULL,         0,            True },
-	{ NULL,            NULL,         "floating",   0,            True },
-	// { "Iceweasel",     NULL,         NULL,         1 << 9,       False },
-	// { "google-chrome", NULL,         NULL,         1 << 9,       False },
-	// { NULL,            NULL,         NULL,         0,            False },
+static const Rule rules[] = {
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ NULL,       NULL,       "floating", 0,            True,        -1 },
 };
 
 /* layout(s) */
-static float mfact      = 0.5; /* factor of master area size [0.05..0.95] */
-static Bool resizehints = True; /* False means respect size hints in tiled resizals */
+static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster      = 1;    /* number of clients in master area */
+static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
-static Layout layouts[] = {
+static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
@@ -58,38 +51,46 @@ static Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { "uxterm", NULL };
 static const char *termfloatcmd[]  = { "urxvt", "-T", "floating", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
-static const char *chromecmd[]  = { "google-chrome", NULL };
+static const char *chromecmd[]  = { "chromium", "--enable-sync", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = termfloatcmd } },
+	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = firefoxcmd } },
+	{ MODKEY|ControlMask,           XK_b,      spawn,          {.v = chromecmd } },
+	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask,           XK_c,      spawn,          {.v = chromecmd } },
-	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = firefoxcmd } },
-	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
-	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = termfloatcmd } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -99,11 +100,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
-/* click can be a tag number (starting at 0),
- * ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
