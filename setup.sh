ae85fe74a0647d0ff81e244d1c05341308f2f6c1
Lorenzo Bolla <lbolla@gmail.com>
2013-08-27 10:07:28 +0100

    some configs

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ function link {
 	ln -sf $HOME/src/dotfiles/$1 $HOME/
 }
 
-# for x in .bash .bashrc .vim .vimrc .Xdefaults .inputrc; do
-for x in .vim .vimrc .Xdefaults .zshrc .git-prompt.sh .gitconfig .jsl.conf .ghc; do
+for x in .bash .bashrc .vim .vimrc .Xdefaults .zshrc .git-prompt.sh .gitconfig .jsl.conf .ghc .sqshrc; do
 	link $x
 done
