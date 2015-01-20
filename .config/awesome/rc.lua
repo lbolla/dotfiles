3259202f7487ffd762a3869dae20916a80020495
Lorenzo Bolla <lbolla@gmail.com>
2015-01-20 11:34:36 +0000

    Enable port 6000 in chrome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -288,7 +288,7 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey,           }, "b",      function () awful.util.spawn("firefox") end),
-    awful.key({ modkey,           }, "c",      function () awful.util.spawn("google-chrome") end),
+    awful.key({ modkey,           }, "c",      function () awful.util.spawn("google-chrome --explicitly-allowed-ports=6000") end),
     awful.key({ modkey,           }, "e",      function () awful.util.spawn("emacs") end),
     awful.key({ modkey,           }, "t",      function () awful.util.spawn("thunderbird") end),
     -- awful.key({ modkey, "Control" }, "r",      awesome.restart),
