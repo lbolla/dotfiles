008f27978bb67572dbde40e4e98540dbe84083e7
Lorenzo Bolla <lbolla@gmail.com>
2015-04-11 23:17:41 +0100

    Added config for pdbpp

---
+++ .pdbrc.py
@@ -0,0 +1,4 @@
+import pdb
+
+class Config(pdb.DefaultConfig):
+    sticky_by_default = True
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ function link {
 	ln -sf $HOME/src/dotfiles/$1 $HOME/$1
 }
 
-#for x in .bash .bashrc .vim .vimrc .Xdefaults .zshrc .git-prompt.sh .gitconfig .jsl.conf .ghc .sqshrc .config/ipython .xmonad .tmux.conf .pyrc; do
+#for x in .bash .bashrc .vim .vimrc .Xdefaults .zshrc .git-prompt.sh .gitconfig .jsl.conf .ghc .sqshrc .config/ipython .xmonad .tmux.conf .pyrc .pdbrc.py; do
 for x in .git-prompt.sh; do
 	link $x
 done
