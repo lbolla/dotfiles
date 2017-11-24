fb529cd33bd77ec8bb7daa41f2de7bfa8fe3cb1b
Lorenzo Bolla <lbolla@gmail.com>
2017-11-24 09:06:02 +0000

    Remove unused awesomewm layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -79,18 +79,18 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.floating,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.tile,
+    -- awful.layout.suit.floating,
     -- awful.layout.suit.tile.left,
-	awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
+    -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
- -- awful.layout.suit.magnifier
+    -- awful.layout.suit.max,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier
 }
 -- }}}
 
@@ -99,8 +99,8 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({"一", "二", "三", "四", "五", "六", "七", "八", "九"}, s, layouts[2])
-    -- tags[s] = awful.tag({"1", "2", "3", "4"}, s, layouts[2])
+    tags[s] = awful.tag({"一", "二", "三", "四", "五", "六", "七", "八", "九"}, s, layouts[1])
+    -- tags[s] = awful.tag({"1", "2", "3", "4"}, s, layouts[1])
 end
 -- }}}
 
@@ -359,6 +359,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "e",      function () awful.util.spawn("emacs") end),
     -- awful.key({ modkey,           }, "t",      function () awful.util.spawn("thunderbird") end),
     -- awful.key({ modkey, "Shift"   }, "q",      awesome.quit),
+    awful.key({ modkey,           }, "s",      function () awful.util.spawn("slack") end),
     awful.key({ modkey, "Shift"   }, "s",      function () awful.util.spawn(lock) end),
     awful.key({ modkey, "Shift"   }, "q",      function () awful.util.spawn(poweroff) end),
     awful.key({ modkey,           }, "F1",     function () awful.util.spawn(control_panel) end),
