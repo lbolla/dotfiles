e19dbdd501b491ab8c5709a8fb9bb002b32e72fe
Lorenzo Bolla <lbolla@gmail.com>
2018-04-29 09:03:22 +0100

    fzf setup

--- .oh-my-zsh/custom/plugins/fzf/fzf.plugin.zsh
+++ .oh-my-zsh/custom/plugins/fzf/fzf.plugin.zsh
@@ -1,24 +1,19 @@
-if type "fzf" > /dev/null
+if type "rg" > /dev/null
 then
+    export FZF_DEFAULT_COMMAND='rg --files'
+fi
 
-    if type "rg" > /dev/null
-    then
-        export FZF_DEFAULT_COMMAND='rg --files'
-    fi
+if [[ -f $HOME/.fzf.zsh ]]
+then
+    source $HOME/.fzf.zsh
+fi
 
-    if [[ -f $HOME/.fzf.zsh ]]
-    then
-        source $HOME/.fzf.zsh
-    fi
+if [[ -f /usr/share/doc/fzf/completion.zsh ]]
+then
+    source /usr/share/doc/fzf/completion.zsh
+fi
 
-    if [[ -f /usr/share/doc/fzf/completion.zsh ]]
-    then
-        source /usr/share/doc/fzf/completion.zsh
-    fi
-
-    if [[ -f /usr/share/doc/fzf/key-bindings.zsh ]]
-    then
-        source /usr/share/doc/fzf/key-bindings.zsh
-    fi
-
+if [[ -f /usr/share/doc/fzf/key-bindings.zsh ]]
+then
+    source /usr/share/doc/fzf/key-bindings.zsh
 fi
