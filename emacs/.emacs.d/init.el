e0aac766101af40870792641132d724d53c891fe
Lorenzo Bolla <lbolla@gmail.com>
2018-05-17 10:02:57 +0100

    Prefer quasi-monochrome on void

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -270,9 +270,9 @@
 ;;             (load-theme-quasi-monochrome)
 ;;             ))
 
-(load-theme-cyberpunk)
+;; (load-theme-cyberpunk)
 ;; (load-theme-leuven)
-;; (load-theme-quasi-monochrome)
+(load-theme-quasi-monochrome)
 ;; (load-theme-zerodark)
 
 (pinentry-start)
