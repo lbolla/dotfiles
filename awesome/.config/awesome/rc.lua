5fa825de538cba1638de483880d608f34c4e2b7a
Lorenzo Bolla <lbolla@gmail.com>
2018-05-20 14:27:37 +0100

    Awesome key bindings

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -284,6 +284,10 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey,           }, "b", function () awful.spawn("firefox") end,
+              {description = "open firefox", group = "launcher"}),
+    awful.key({ modkey,           }, "e", function () awful.spawn(emacs_cmd) end,
+              {description = "open emacs", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
