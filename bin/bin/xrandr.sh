d66a7d9238f8cbf35436aa03596b65dc708cde82
Lorenzo Bolla <lbolla@gmail.com>
2018-07-06 08:08:12 +0100

    Xrandr script

---
+++ bin/bin/xrandr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dmode="$(cat /sys/class/drm/card0-VGA-1/status)"
+
+export DISPLAY=:0
+export XAUTHORITY=/home/lbolla/.Xauthority
+
+if [ "${dmode}" = disconnected ]; then
+     /usr/bin/xrandr --auto
+elif [ "${dmode}" = connected ];then
+     /usr/bin/xrandr --output VGA1 --auto --right-of LVDS1
+else /usr/bin/xrandr --auto
+fi
+
