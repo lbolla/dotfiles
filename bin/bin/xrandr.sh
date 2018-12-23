017d16d9a1ba052a5147dace16839fa907dfbce5
Lorenzo Bolla <lbolla@gmail.com>
2018-12-23 18:05:03 +0000

    Useful bin commands

--- bin/bin/xrandr.sh
+++ bin/bin/xrandr.sh
@@ -5,10 +5,15 @@ dmode="$(cat /sys/class/drm/card0-VGA-1/status)"
 export DISPLAY=:0
 export XAUTHORITY=/home/lbolla/.Xauthority
 
-if [ "${dmode}" = disconnected ]; then
+if [ "${dmode}" = disconnected ]
+then
      /usr/bin/xrandr --auto
-elif [ "${dmode}" = connected ];then
-     /usr/bin/xrandr --output VGA1 --auto --right-of LVDS1
-else /usr/bin/xrandr --auto
+elif [ "${dmode}" = connected ]
+then
+     /usr/bin/xrandr --output VGA1 --primary --auto --right-of LVDS1
+else
+    /usr/bin/xrandr --auto
 fi
 
+# Restart wm
+pkill -1 awesome
