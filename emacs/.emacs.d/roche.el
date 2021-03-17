dcb0bec4b227dd9cff7ffcfd6950d284b4fc6a08
Lorenzo Bolla <code@lbolla.info>
2021-03-17 08:01:49 +0100

    emacs: mu4e for roche

--- emacs/.emacs.d/roche.el
+++ emacs/.emacs.d/roche.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+(require 'private-roche "~/Private/emacs/private-roche.el")
+
 (defvar org-agenda-files)
 (defvar org-link-abbrev-alist)
 
@@ -27,6 +29,92 @@
   :after magit
   :config
   (add-to-list 'forge-alist '("ssh.code.roche.com" "code.roche.com/api/v4" "code.roche.com" forge-gitlab-repository)))
+
+(use-package mu4e
+  :load-path "/usr/local/share/emacs/site-lisp/mu4e/"
+  :ensure nil  ;; installed system-wide
+  ;; :demand t
+
+  :defines
+  mu4e-view-actions
+  roche-smtp-user
+
+  :functions
+  my/mu4e-headers-narrow-thing-at-point
+  my/mu4e-refresh-headers
+
+  :bind (("C-c m m" . mu4e)
+         ("C-c m n" . mu4e-compose-new)
+         :map mu4e-main-mode-map
+         ("C-c C-u" . my/mu4e-refresh-headers)
+         :map mu4e-headers-mode-map
+         ("C-c /" . my/mu4e-headers-narrow)
+         ("C-c C-u" . my/mu4e-refresh-headers)
+         ("<backspace>" . mu4e-headers-query-prev))
+
+  :custom
+  (user-mail-address roche-smtp-user)
+  (user-full-name  "Lorenzo Bolla")
+  (mu4e-drafts-folder "/Roche/[Gmail]/Drafts")
+  (mu4e-sent-folder "/Roche/[Gmail]/Sent Mail")
+  (mu4e-sent-messages-behavior 'delete)
+  (mu4e-trash-folder "/Roche/[Gmail]/Trash")
+  (mu4e-change-filenames-when-moving t)
+  (mu4e-maildir-shortcuts '((:maildir "/Roche/Inbox"             :key ?i)
+                            (:maildir "/Roche/Gitlab"            :key ?g)
+                            (:maildir "/Roche/Bitbucket"         :key ?b)
+                            (:maildir "/Roche/[Gmail]/Sent Mail" :key ?s)
+                            (:maildir "/Roche/[Gmail]/All Mail"  :key ?a)
+                            (:maildir "/Roche/[Gmail]/Trash"     :key ?t)))
+  (mu4e-bookmarks '((:name "Unread"               :query "flag:unread AND NOT flag:trashed AND NOT maildir:/Roche/[Gmail]/*"                 :key ?u)
+                    (:name "Unread Inbox"         :query "flag:unread AND NOT flag:trashed AND maildir:/Roche/Inbox"                         :key ?i)
+                    (:name "Starred"              :query "flag:flagged"                                                                       :key ?f)
+                    ;; (:name "Starred"              :query "flag:flagged AND NOT maildir:/Roche/[Gmail]/*"                                     :key ?f)
+                    ;; (:name "Starred"              :query "maildir:/Roche/[Gmail]/Starred"       :key ?f)
+                    ))
+  ;; (mail-user-agent 'mu4e-user-agent)
+  (mu4e-compose-complete-addresses t)
+  (mu4e-compose-complete-only-after nil)
+  (mu4e-compose-complete-only-personal nil)
+  (mu4e-compose-dont-reply-to-self t)
+  (mu4e-use-fancy-chars nil)
+  (mu4e-get-mail-command "true")
+  (mu4e-update-interval 300)
+  ;; TODO
+  ;; (mu4e-html2text-command "w3m -dump -cols 80 -T text/html")
+  (mu4e-view-html-plaintext-ratio-heuristic 20)
+  (message-kill-buffer-on-exit t)
+  (mu4e-view-scroll-to-next nil)
+  (mu4e-attachment-dir "/tmp")
+  (mu4e-headers-skip-duplicates t)
+  (mu4e-headers-date-format "%x %X")
+  (mu4e-headers-fields '((:human-date . 18)
+                         (:flags . 6)
+                         (:mailing-list . 10)
+                         (:from . 22)
+                         (:subject)))
+  (mu4e-mailing-list-patterns '("\\([^.]*\\)\\.roche\\.com"))
+  (mu4e-headers-include-related nil)
+  ;; (mu4e-headers-results-limit 500)
+  (mu4e-view-actions '(("capture message" . mu4e-action-capture-message)
+                       ("view as pdf" . mu4e-action-view-as-pdf)
+                       ("show this thread" . mu4e-action-show-thread)
+                       ("browse" . mu4e-action-view-in-browser)))
+  (mu4e-view-show-addresses t)
+  (mu4e-view-show-images nil)
+  (mu4e-view-use-gnus nil))
+
+(use-package smtpmail
+  :defines
+  roche-smtp-server
+  roche-smtp-port
+  roche-smtp-user
+  :custom
+  (send-mail-function 'smtpmail-send-it)
+  (smtpmail-smtp-server roche-smtp-server)
+  (smtpmail-smtp-service roche-smtp-port)
+  (smtpmail-mail-address roche-smtp-user)
+  (smtpmail-stream-type 'starttls))
 
 (defun roche-gitlab-object-url (tag)
   "Generate a Roche GitLab OBJECT url for TAG."
