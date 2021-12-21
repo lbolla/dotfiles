80268db0cd87e786fe37cd521900d74056aafc92
Lorenzo Bolla <code@lbolla.info>
2021-12-21 14:40:51 +0100

    emacs: make C-w delete backward word if region is not selected

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -51,6 +51,14 @@ See https://stackoverflow.com/questions/234963/re-open-scratch-buffer-in-emacs#2
         (bury-buffer)
       ad-do-it)))
 
+;; https://www.emacswiki.org/emacs/KillingAndYanking
+(defadvice kill-region (before unix-werase activate compile)
+  "When called interactively with no active region, delete a single word
+    backwards instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (save-excursion (backward-word 1) (point)) (point)))))
+
 ;; Show buffer path, relative to project root
 ;; https://www.reddit.com/r/emacs/comments/8xobt3/tip_in_modeline_show_buffer_file_path_relative_to/
 (with-eval-after-load 'subr-x
