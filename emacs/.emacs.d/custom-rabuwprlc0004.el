6579f94259dedb12a843bc68d491fa7016ba6b4a
Lorenzo Bolla <code@lbolla.info>
2022-01-03 08:48:16 +0100

    emacs: config for rabuwprlc0004

---
+++ emacs/.emacs.d/custom-rabuwprlc0004.el
@@ -0,0 +1,22 @@
+;;; custom-rabuwprlc0004.el ---                      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Lorenzo Bolla
+
+;; Author: Lorenzo Bolla <lorenzo.bolla@contractors.roche.com>
+;; Keywords: 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(zoom-window yasnippet-snippets yaml-mode which-key web-mode use-package typescript-mode treemacs-projectile treemacs-magit toml-mode terraform-mode tao-theme smartscan rust-mode rjsx-mode rg qml-mode protobuf-mode poetry poet-theme pass org-ql nim-mode modus-themes minions magit-todos lua-mode lsp-ui lsp-python-ms lsp-java lsp-haskell leuven-theme keyfreq json-mode ivy-hydra highlight-indentation groovy-mode goose-theme go-mode forge flycheck-rust flycheck-pycheckers flycheck-color-mode-line flycheck-checkbashisms eyebrowse expand-region exec-path-from-shell evil-org evil-nerd-commenter evil-collection elixir-mode edit-server dumb-jump dpkg-dev-el doom-themes dockerfile-mode cython-mode cyberpunk-theme csv-mode csharp-mode counsel company clang-format all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka" :weight light :height 120))))
+ '(fixed-pitch ((t (:family "Iosevka" :height 120))))
+ '(fixed-pitch-serif ((t (:family "FreeMono" :height 120))))
+ '(variable-pitch ((t (:family "Gentium" :height 140)))))
