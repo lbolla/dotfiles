25b6d2d0c304e5ccfe9d6d8e7e914a5d507bccaa
Lorenzo Bolla <code@lbolla.info>
2022-01-08 21:09:34 +0100

    gnus: simplify setup

--- emacs/.gnus.el
+++ emacs/.gnus.el
@@ -4,20 +4,10 @@
 
 ;;; Code:
 
-;;@see http://www.emacswiki.org/emacs/GnusGmail#toc1
-;;@see http://www.emacswiki.org/emacs/GnusTutorial
-;;@see https://github.com/redguardtoo/mastering-emacs-in-one-year-guide/blob/master/gnus-guide-en.org
-;;@see https://github.com/drewr/dotemacs/blob/master/d/gnus.el
-;;@see https://github.com/jwiegley/dot-emacs/blob/master/dot-gnus.el
-
 (use-package gnus
   :custom
-  (gnus-select-method '(nntp "nntp.aioe.org"))
-  (gnus-summary-line-format "%d %U%R%z%I%(%[%4L: %-23,23f%]%) %s\n")
-  :defines
-  gnus-secondary-select-methods
-  :config
-  (add-to-list 'gnus-secondary-select-methods '(nntp "news.gmane.org")))
+  (gnus-select-method '(nntp "news.gmane.io"))
+  (gnus-summary-line-format "%d %U%R%z%I%(%[%4L: %-23,23f%]%) %s\n"))
 
 (provide '.gnus)
 ;;; .gnus ends here
